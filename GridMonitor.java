061ed9f41fa53dbb4f302ecee3e84562acb36ecb
Oliver Hill <Oliverhill@u.boisestate.edu>
2025-01-16 15:24:21 -0700

    First GridMonitor commit 1/16/25

--- GridMonitor.java
+++ GridMonitor.java
@@ -1,4 +1,40 @@
-public class GridMonitor 
+
+import java.io.FileNotFoundException;
+
+public class GridMonitor implements GridMonitorInterface 
 {
     
+    private String filename;
+
+
+    public GridMonitor(String filename) throws FileNotFoundException
+    {
+        this.filename = filename;
+    }
+
+    public double[][] getBaseGrid()
+    {
+
+    }
+
+    public double[][] getSurroundingSumGrid()
+    {
+
+    }
+
+    public double[][] getSurroundingAvgGrid()
+    {
+        
+    }
+
+    public double[][] getDeltaGrid()
+    {
+
+    }
+
+    public boolean[][] getDangerGrid()
+    {
+        
+    }
+
 }
