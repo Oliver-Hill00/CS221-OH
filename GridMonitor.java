c33217d8181bdbe1510a599383817f99ca946549
Oliver Hill <Oliverhill@u.boisestate.edu>
2025-01-16 19:23:58 -0700

    1/16 Commit #2

--- GridMonitor.java
+++ GridMonitor.java
@@ -1,27 +1,62 @@
-
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class GridMonitor implements GridMonitorInterface 
+public class GridMonitor implements GridMonitorInterface
 {
     
-    private String filename;
+    private double[][] = baseGrid;
+    private double[][] = surroundingSumGrid;
+    private double[][] = surroundingAvgGrid;
+    private double[][] = deltaGrid;
+    private boolean[][] = dangerGrid;
 
-
+    
     public GridMonitor(String filename) throws FileNotFoundException
     {
-        this.filename = filename;
+        loadGrid(filename);
+    }
+
+    private void loadGrid(String filename) throws FileNotFoundException
+    {
+        Scanner scanner = new Scanner(new File(filename));
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+        baseGrid = new double[rows][cols];
+
+        for(int i = 0; i < rows; i++)
+        {
+            for(int k = 0; k < cols; k++)
+            {
+                baseGrid[i][k] = scanner.nextDouble();
+            } 
+        }
+
+        scanner.close();
+
     }
 
     public double[][] getBaseGrid()
     {
-
+        return baseGrid;
     }
 
     public double[][] getSurroundingSumGrid()
     {
-
+        if(surroundingSumGrid == null)
+        {
+            calculateSurroundingSumGrid;
+        }
+        return surroundingSumGrid;
     }
 
+    public void calculateSurroundingSumGrid()
+    {
+        int rows = baseGrid.length;
+        int cols = basegrid[0].length;
+        surroundingSumGrid = new double[rows][cols];
+    }
+   
     public double[][] getSurroundingAvgGrid()
     {
         
@@ -34,7 +69,7 @@ public class GridMonitor implements GridMonitorInterface
 
     public boolean[][] getDangerGrid()
     {
-        
+
     }
 
 }
